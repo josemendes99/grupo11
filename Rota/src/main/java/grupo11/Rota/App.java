87f232c1af2adbe6237b62e26fe7e4213975caf6
User <User@eduardo>
2021-05-26 18:33:18 -0300

    inicial

---
+++ Rota/src/main/java/grupo11/Rota/App.java
@@ -0,0 +1,13 @@
+package grupo11.Rota;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
